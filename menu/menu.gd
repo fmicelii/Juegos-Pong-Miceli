f6c403340a3287261e783b5cae15bb325389c22f
Fran <franmiceli08@gmail.com>
2026-03-24 21:05:53 -0300

    agregamos algunos efectos de sonido y cambiamos las paletas

--- menu/menu.gd
+++ menu/menu.gd
@@ -1,12 +1,25 @@
 extends Control
 
+# Referencias a los contenedores
+@onready var menu_principal = $MenuPrincipalContenedor # O el nombre que le hayas puesto
+@onready var opciones = $OpcionesContenedor
 
 func _on_jugar_pressed() -> void:
-	get_tree().change_scene_to_file("res://pong/pong.tscn")
+	if get_tree().paused:
+		get_tree().paused = false
+		get_parent().queue_free()
+	else:
+		get_tree().change_scene_to_file("res://pong/pong.tscn")
 
 func _on_opciones_pressed() -> void:
-	get_tree().change_scene_to_file("res://menu/opciones.tscn")
+	# En lugar de cambiar escena, ocultamos uno y mostramos otro
+	menu_principal.visible = false
+	opciones.visible = true
 
+func _on_atras_pressed() -> void:
+	# El botón de atrás ahora hace lo inverso
+	opciones.visible = false
+	menu_principal.visible = true
 
 func _on_salir_pressed() -> void:
 	get_tree().quit()
--- pong/pong.gd
+++ pong/pong.gd
@@ -11,6 +11,8 @@ func _on_arco_izquierda_body_entered(body: Node2D) -> void:
 		$marcador2.text = str(puntosJugador2)
 		body.reset_pelota()
 		print("gol! anotan los rojos")
+		$fahh.play()
+
 
 
 func _on_arco_derecha_body_entered(body: Node2D) -> void:
@@ -19,3 +21,40 @@ func _on_arco_derecha_body_entered(body: Node2D) -> void:
 		$marcador1.text = str(puntosJugador1)
 		body.reset_pelota()
 		print("gol! la metieron los azules")
+		$fahh.play()
+#config menu
+
+var menu_escena = preload("res://menu/menu.tscn")
+var menu_instancia = null
+
+func _process(_delta):
+	if Input.is_action_just_pressed("pausa"):
+		abrir_menu()
+
+func abrir_menu():
+	if menu_instancia == null:
+		# Creamos una capa nueva para que el menú flote sobre el juego
+		var capa_menu = CanvasLayer.new()
+		capa_menu.name = "CapaMenu"
+		
+		# Instanciamos tu menú
+		menu_instancia = menu_escena.instantiate()
+		
+		# Añadimos el menú a la capa, y la capa a la escena
+		capa_menu.add_child(menu_instancia)
+		add_child(capa_menu)
+		
+		# Forzamos a que el Control ocupe toda la capa (centrado automático)
+		if menu_instancia is Control:
+			menu_instancia.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
+		
+		get_tree().paused = true
+	else:
+		# cerramos la instancia si estaba abierta
+		cerrar_menu()
+
+func cerrar_menu():
+	if menu_instancia != null:
+		menu_instancia.queue_free()
+		menu_instancia = null
+		get_tree().paused = false
